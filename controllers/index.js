be65a7a05bf6d29535e1852d58552fb1d0ee2cd1
andymcg303 <mcgow_a@hotmail.com>
2020-04-23 23:38:53 +0100

    Forgot/Reset Password functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 const passport = require('passport');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
 const util = require('util');
 const { cloudinary } = require('../cloudinary');
 const { deleteProfileImage } = require('../middleware');
-const mapBoxToken = process.env.MAPBOX_TOKEN;
+const crypto = require('crypto');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = {
 	// GET /
@@ -12,21 +15,16 @@ module.exports = {
 		const posts = await Post.find({});
 		res.render('index', { posts, mapBoxToken, title: 'Surf Shop - Home' });
 	},
-
 	// GET /register
 	getRegister(req, res, next) {
 		res.render('register', { title: 'Register', username: '', email: '' });
 	},
-
 	// POST /register
 	async postRegister(req, res, next) {
 		try {
 			if (req.file) {
 				const { secure_url, public_id } = req.file;
-				req.body.image = {
-					secure_url,
-					public_id
-				}
+				req.body.image = { secure_url, public_id };
 			}
 			const user = await User.register(new User(req.body), req.body.password);
 			req.login(user, function(err) {
@@ -35,28 +33,26 @@ module.exports = {
 				res.redirect('/');
 			});
 		} catch(err) {
+			deleteProfileImage(req);
 			const { username, email } = req.body;
 			let error = err.message;
 			if (error.includes('duplicate') && error.includes('index: email_1 dup key')) {
 				error = 'A user with the given email is already registered';
 			}
-			res.render('register', { title: 'Register', username, email, error })
+			res.render('register', { title: 'Register', username, email, error });
 		}
 	},
-
 	// GET /login
 	getLogin(req, res, next) {
 		if (req.isAuthenticated()) return res.redirect('/');
 		if (req.query.returnTo) req.session.redirectTo = req.headers.referer;
 		res.render('login', { title: 'Login' });
 	},
-
+	// POST /login
 	async postLogin(req, res, next) {
 		const { username, password } = req.body;
 		const { user, error } = await User.authenticate()(username, password);
-		if(!user && error) {
-			return next(error);
-		}
+		if (!user && error) return next(error);
 		req.login(user, function(err) {
 			if (err) return next(err);
 			req.session.success = `Welcome back, ${username}!`;
@@ -65,29 +61,21 @@ module.exports = {
 			res.redirect(redirectUrl);
 		});
 	},
-
 	// GET /logout
 	getLogout(req, res, next) {
 	  req.logout();
 	  res.redirect('/');
 	},
-	
-	// GET /profile
 	async getProfile(req, res, next) {
-	  const posts = await Post.find().where('author').equals(req.user._id).limit(10).exec();
-	  res.render('profile', { posts });
+		const posts = await Post.find().where('author').equals(req.user._id).limit(10).exec();
+		res.render('profile', { posts });
 	},
-
-	// UPDATE /profile
 	async updateProfile(req, res, next) {
-		// destructure username and email from req.body
 		const {
 			username,
 			email
 		} = req.body;
-		// destructure user object from res.locals
 		const { user } = res.locals;
-		// check if username or email need to be updated
 		if (username) user.username = username;
 		if (email) user.email = email;
 		if (req.file) {
@@ -95,14 +83,95 @@ module.exports = {
 			const { secure_url, public_id } = req.file;
 			user.image = { secure_url, public_id };
 		}
-		// save the updated user to the database
 		await user.save();
-		// promsify req.login
 		const login = util.promisify(req.login.bind(req));
-		// log the user back in with new info
 		await login(user);
-		// redirect to /profile with a success flash message
 		req.session.success = 'Profile successfully updated!';
 		res.redirect('/profile');
+	},
+	getForgotPw(req, res, next) {
+		res.render('users/forgot');
+	},
+	async putForgotPw(req, res, next) {
+		const token = await crypto.randomBytes(20).toString('hex');
+		const { email } = req.body;
+		const user = await User.findOne({ email });
+		if (!user) {
+			req.session.error = 'No account with that email.';
+			return res.redirect('/forgot-password');
+		}
+		user.resetPasswordToken = token;
+		user.resetPasswordExpires = Date.now() + 3600000;
+		await user.save();
+
+		const msg = {
+		  to: email,
+		  from: 'mcgow_a@hotmail.com',
+		  subject: 'Surf Shop - Forgot Password / Reset',
+		  text: `You are receiving this because you (or someone else)
+		  have requested the reset of the password for your account.
+			Please click on the following link, or copy and paste it
+			into your browser to complete the process:
+			http://${req.headers.host}/reset/${token}
+			If you did not request this, please ignore this email and
+			your password will remain unchanged.`.replace(/		  /g, ''),
+		  // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+		};
+		await sgMail.send(msg);
+
+		req.session.success = `An email has been sent to ${email} with further instructions.`;
+		res.redirect('/forgot-password');
+	},
+	async getReset(req, res, next) {
+		const { token } = req.params;
+		const user = await User.findOne({
+			resetPasswordToken: token,
+			resetPasswordExpires: { $gt: Date.now() }
+		});
+
+		if (!user) {
+			req.session.error = 'Password reset token is invalid or has expired.';
+			return res.redirect('/forgot-password');
+		}
+
+		res.render('users/reset', { token });
+	},
+	async putReset(req, res, next) {
+		const { token } = req.params;
+		const user = await User.findOne({
+			resetPasswordToken: token,
+			resetPasswordExpires: { $gt: Date.now() }
+		});
+
+		if (!user) {
+			req.session.error = 'Password reset token is invalid or has expired.';
+			return res.redirect('/forgot-password');
+		}
+
+		if (req.body.password === req.body.confirm) {
+			await user.setPassword(req.body.password);
+			user.resetPasswordToken = null;
+			user.resetPasswordExpires = null;
+			await user.save();
+			const login = util.promisify(req.login.bind(req));
+			await login(user);
+		} else {
+			req.session.error = 'Passwords do not match.';
+			return res.redirect(`/reset/${ token }`);
+		}
+
+		const msg = {
+	    to: user.email,
+	    from: 'mcgow_a@hotmail.com',
+	    subject: 'Surf Shop - Password Changed',
+	    text: `Hello,
+		  This email is to confirm that the password for your account has just been changed.
+		  If you did not make this change, please hit reply and notify us at once.`.replace(/		  /g, '')
+	  };
+
+	  await sgMail.send(msg);
+
+	  req.session.success = 'Password successfully updated!';
+	  res.redirect('/');
 	}
 }
--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
 	email: { type: String, unique: true, required: true },
-	image: {
+	image:  {
 		secure_url: { type: String, default: '/images/default-profile.jpg' },
 		public_id: String
-	}
+	},
+	resetPasswordToken: String,
+	resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
